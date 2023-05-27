7587bde137bd53b20cec12974a63c566058a24c5
Felicia Ahlström <fellan@MacBook-Air-som-tillhor-Felicia.local>
2023-05-27 11:33:26 +0200

    added functions getQuizAnswers and postQuizAnswers

--- src/App.js
+++ src/App.js
@@ -9,28 +9,16 @@ const connection = mysql.createConnection({
     password: '',
 });
 
-//ES6 = () => {}
-//ES5 = function () {}
-
-// function qqq (sql, cb){
-//     const result = async callDatabase(sql);
-//     cb('err', result);
-//     cleanUpDatabaseConnection();
-//
-// }
-
-
-function bajs(thisIsTotallyNot123, y, z){
-    console.log(thisIsTotallyNot123)
-}
-
 const app = express();
 const port = 3001;
 app.use(cors());
+app.use(express.json());
 
 app.get('/getQuizQuestions', (req, res) => {
     connection.connect((err) => {
-        if (err) throw err;
+        if (err){
+            throw err;
+        }
         connection.query('SELECT * FROM questions', (err, result) => {
             if (err) throw err;
             res.send(result);
@@ -41,15 +29,78 @@ app.get('/getQuizQuestions', (req, res) => {
 app.get('/getQuizChoices', (req, res) => {
     connection.connect((err) => {
         if (err) throw err;
-        connection.query('SELECT * FROM choices', (err, result) => {
+        const sql = 'SELECT * FROM choices'
+        connection.query(sql, (err, result) => {
             if (err) throw err;
             res.send(result);
         })
     })
 });
 
+app.post('/postQuizAnswers', (req, res) => {
+    const {result} = req.body;
+    const resArr = (Object.entries(result));
 
-// localhost:3001/getQuizQuestions
+    const finalResult = resArr.map(el => {
+        return el[1].choice;
+    }).join();
+
+    connection.connect((err) => {
+        if (err) throw err;
+        const sql = 'INSERT INTO answers VALUES (id, ?)';
+        connection.query(sql, [finalResult], (err, result) => {
+            if (err) throw err;
+        })
+        res.send()
+    })
+})
+
+app.get('/getQuizAnswers', (req, res) => {
+    connection.connect((err) => {
+        if (err) throw err;
+        const sql = 'SELECT answers FROM answers'
+        connection.query(sql, (err, result) => {
+            if (err) throw err;
+            const tempArr0 = [];
+            const tempArr1 = [];
+            const tempArr2 = [];
+            const tempArr3 = [];
+            const tempArr4 = [];
+
+            result.forEach(({answers}) => {
+                answers.split(',').forEach((el, index) => {
+
+                    switch(index){
+                        case 0:
+                            tempArr0.push(el);
+                            break;
+                        case 1:
+                            tempArr1.push(el);
+                            break;
+                        case 2:
+                            tempArr2.push(el);
+                            break;
+                        case 3:
+                            tempArr3.push(el);
+                            break;
+                        case 4:
+                            tempArr4.push(el);
+                            break;
+                    }
+                })
+            })
+
+            const allAnswersArr = [];
+            allAnswersArr.push(tempArr0, tempArr1, tempArr2, tempArr3, tempArr4);
+            console.log(allAnswersArr)
+
+            console.log(tempArr0, tempArr1, tempArr2, tempArr3 && tempArr4);
+            // console.log(result);
+            res.send(allAnswersArr);
+        })
+    })
+})
+
 app.listen(3001, () => {
     console.log(`server running on port ${port}`);
 });
